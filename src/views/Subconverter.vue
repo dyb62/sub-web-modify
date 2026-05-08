08f1b36de2c2344efd79cdecbb0cd9690734b5ee
dyb62 <dykenb62@outlook.com>
2026-05-08 12:13:50 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -339,7 +339,7 @@ export default {
       btnBoolean: false,
       options: {
         clientTypes: { Clash: "clash", "Surge4/5": "surge&ver=4", "Sing-Box": "singbox", V2Ray: "v2ray", Trojan: "trojan", ShadowsocksR: "ssr", "混合订阅（mixed）": "mixed", Surfboard: "surfboard", Quantumult: "quan", "Quantumult X": "quanx", Loon: "loon", Mellow: "mellow", Surge3: "surge&ver=3", Surge2: "surge&ver=2", ClashR: "clashr", "Shadowsocks(SIP002)": "ss", "Shadowsocks Android(SIP008)": "sssub", ShadowsocksD: "ssd", "自动判断客户端": "auto" },
-        shortTypes: { "v1.mk": "https://v1.mk/short", "d1.mk": "https://d1.mk/short", "dlj.tf": "https://dlj.tf/short", "suo.yt": "https://suo.yt/short" },
+        shortTypes: { "myurl": "https://myurl.7li7li.com/short", "zrf": "https://d.001315.xyz/short" },
         customBackend: { "周润发HK后端【由YXVM赞助服务】": "https://subapi.zrfme.com", "自营后端": "https://subapi.xunyou.de5.net","7li7li": "https://subapi.7li7li.com" },
         remoteConfig: [
           {
